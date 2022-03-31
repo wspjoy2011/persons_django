fb723711d5a1d26e342b56cb1d368d8e9c37a83e
wspjoy <wspjoy2011@gmail.com>
2022-03-31 20:11:20 +0300

    Initial project

--- persons/base/urls.py
+++ persons/base/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('categories/', categories, name='categories'),
+    path('categories/<int:pk>/', categories, name='categories'),
 ]
--- persons/base/views.py
+++ persons/base/views.py
@@ -6,5 +6,5 @@ def index(request):
     return HttpResponse("<h1>Main page.</h1>")
 
 
-def categories(request):
-    return HttpResponse("<h1>Categories page.</h1>")
+def categories(request, pk):
+    return HttpResponse(f"<h1>Category {pk}.</h1>")
